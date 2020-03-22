18eaabf7d4fdccc680e02459782591e420365c5d
noah <noahlog@naver.com>
2020-03-23 08:55:53 +0900

    Initialize project
    Show producer calendar.

---
+++ README.md
@@ -0,0 +1,3 @@
+# producer-calendar
+
+[생산자 달력](https://docs.google.com/spreadsheets/d/1tN7lz3hwGi-um2YTRjjdAUyKJFRrSQO_HHeSO6t-A5A/edit#gid=1493117693)
---
+++ js/application.js
@@ -0,0 +1,38 @@
+var id = '1tN7lz3hwGi-um2YTRjjdAUyKJFRrSQO_HHeSO6t-A5A';
+var url = 'https://spreadsheets.google.com/feeds/list/' + id + '/od6/public/values?alt=json';
+
+axios.get(url)
+  .then(function(response) {
+    var items = response.data.feed.entry;
+    var events = items.map(function(row) {
+      var date = moment(row['gsx$date'].$t, 'YYYY. MM. DD');
+      var author = row['gsx$author'].$t;
+      var title = row['gsx$title'].$t;
+      var url = row['gsx$url'].$t;
+      if (!date.isValid() || !title) {
+        return null;
+      }
+      url = url.match(/^http/) ? url : '';
+      return {
+        start: date.format('YYYY-MM-DD'),
+        title: '[' + author + '] ' + title + (url ? '' : ' [예약]'),
+        url: url
+      };
+    }).filter(function(event) { return event != null; });
+
+    var element = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(element, {
+      defaultDate: moment().format(),
+      events: events,
+      eventClick: function(info) {
+        info.jsEvent.preventDefault();
+        if (info.event.url) {
+          window.open(info.event.url);
+        }
+      }
+    });
+    calendar.render();
+  })
+  .catch(function(error) {
+    alert(error);
+  });
